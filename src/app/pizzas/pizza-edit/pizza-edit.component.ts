c0115a5586ef15740bd369b47b1f1b5878bff49d
Juan Manuel Perales <Juan Manuel Perales>
2017-07-01 10:04:23 +0200

    new solution issue controls

--- src/app/pizzas/pizza-edit/pizza-edit.component.ts
+++ src/app/pizzas/pizza-edit/pizza-edit.component.ts
@@ -17,6 +17,8 @@ export class PizzaEditComponent implements OnInit {
   editMode = false;
   pizzaForm: FormGroup;
 
+  get ingredients() { return this.pizzaForm.get('ingredients'); }
+
   constructor(private route: ActivatedRoute,
               private pizzaService: PizzaService,
               private router: Router,
